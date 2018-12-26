5404f73b7518edf8d93f851c1014cf6683af2c10
moormaster <moormaster@gmx.net>
2018-12-26 21:23:37 +0100

    added mapFirstResolveableHostname()

--- lib_sync_functions.sh
+++ lib_sync_functions.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
 
+mapFirstResolveableHostname() {
+	hostnames=( "$@" )
+
+	for (( i=0; i<${#hostnames[@]}; i++ ))
+	do
+		tokens=( ${hostnames[$i]/:/ } )
+		hostname="${tokens[0]}"
+		mapped="${tokens[1]}"
+
+		if [ "$mapped" == "" ]
+		then
+			mapped="$hostname"
+		fi
+
+		if nslookup $hostname | grep "Name:\s${hostname}\(\..*\)\?$" > /dev/null
+		then
+			echo $mapped
+			return
+		fi
+	done
+
+	echo $mapped
+}
+
 backup()
 {
 	backupdir=_rsync_backup
 	now=$(date +%Y_%m_%d_%H_%M)
 
-	mkdir $2/$backupdir
+	rsyncsource=$1
+	rsynctarget=$2
 
-	rsync -abv --exclude=$backupdir --backup-dir=$backupdir/$now --delete $1 $2
-
-	rmdir --ignore-fail-on-non-empty $2/$backupdir
+	mkdir ${rsynctarget}/${backupdir}
+	rsync -abv --exclude=${backupdir} --backup-dir=${backupdir}/${now} --delete ${rsyncsource} ${rsynctarget}
+	rmdir --ignore-fail-on-non-empty ${rsynctarget}/${backupdir}/${now}
+	rmdir --ignore-fail-on-non-empty ${rsynctarget}/${backupdir}
 }
