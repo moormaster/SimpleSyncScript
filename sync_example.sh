c2938b9a4988dc9d3a559e7dc7d8ca0d260155ae
moormaster <moormaster@gmx.net>
2020-07-19 13:41:51 +0200

    always search lib_sync_functions.sh in the same dir as the sync_*.sh script

--- sync_example.sh
+++ sync_example.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-. lib_sync_functions.sh
+SCRIPTDIR="$( dirname "$( readlink -f "$0" )" )"
+. "$SCRIPTDIR/lib_sync_functions.sh"
 
 backup /home/user/MyDocuments/ /media/USBDisc/MyDocuments/
 backup /home/user/MyMusic/ /media/USBDisc/MyMusic/
